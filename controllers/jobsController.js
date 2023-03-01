5272beae4012ccda102635a639881d6b249ed45d
Shekhartayde <shekhartayde14@gmail.com>
2023-03-02 00:02:42 +0530

    Update jobsController.js

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -2,7 +2,7 @@ const Job=require('../models/jobs')
 const geoCoder=require('../utils/geocoder')
 const ErrorHandler=require('../utils/errorHandler')
 const catchAsyncErrors=require('../middlewares/catchAsyncErrors')
-const APIFilters =require('../utils/apiFilters.js')
+const APIFilters =require('../utils/apifilters')
 const path=require('path')
 const fs=require('fs')
 
